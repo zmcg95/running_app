5a1e4a6eb8cab01337e16ce9ff0b7473b682ef47
zmcg95 <zaid.mcgovern@gmail.com>
2025-11-25 18:24:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from textblob import TextBlob
 
-# ------------------------------
+# -------------------------------
 # STREAMLIT LAYOUT
 # ------------------------------
 st.set_page_config(page_title="YouTube Comment Sentiment Analyzer", layout="wide")
